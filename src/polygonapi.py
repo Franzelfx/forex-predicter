050168ac021efd4a8e0cf03d86c1b0cf77a9bba9
Franzelfx <fabian.franz0596@gmail.com>
2023-01-15 12:06:56 +0100

    EDIT steps_in

--- src/polygonapi.py
+++ src/polygonapi.py
@@ -275,7 +275,7 @@ def get_model_dataset(df, n_steps_out):
     return dataset, _open, test_open, scaler_open
 
 def proceed(pair: str):
-    n_steps_in = 60
+    n_steps_in = 100
     n_steps_out = 30
     # Get the data from the API or from CSV
     df = get_data(pair)
