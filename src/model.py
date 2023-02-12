09c3c914f5bba153cabcbfedecf77da13dc956ba
Franzelfx <fabian.franz0596@gmail.com>
2023-02-12 20:19:16 +0100

    EDIT model/config

--- src/model.py
+++ src/model.py
@@ -47,6 +47,8 @@ class Model:
         model.add(Dense(hidden_neurons, activation="tanh"))
         model.add(Dropout(self._dropout))
         model.add(Dense(hidden_neurons, activation="tanh"))
+        model.add(Dropout(self._dropout))
+        model.add(Dense(hidden_neurons, activation="tanh"))
         model.add(Dense(self._y_train.shape[1]))
         model.build(input_shape=(self._x_train.shape[0], self._x_train.shape[1], self._x_train.shape[2]))
         return model
