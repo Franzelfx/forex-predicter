144a0a1fa3c634261217ca2a1510ac2db4bcc74a
Franzelfx <fabian.franz0596@gmail.com>
2023-02-14 15:36:52 +0100

    EDIT batch size

--- src/testbench/config_tb.py
+++ src/testbench/config_tb.py
@@ -59,6 +59,6 @@ MODEL_PATH = os.path.abspath(os.path.dirname(__file__))
 MODEL_NAME = 'EURUSD_15'
 TEST_EPOCHS = 200
 TEST_NEURONS = 64
-TEST_BATCH_SIZE = 4
+TEST_BATCH_SIZE = 32
 TEST_LEARNING_RATE = 0.0005
 PATH_TEST_RESULTS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "test_results"))
