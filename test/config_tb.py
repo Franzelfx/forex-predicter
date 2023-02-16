d640811317d4f9e0c9a6f92d5a5a7d7b695d57ef
Franzelfx <fabian.franz0596@gmail.com>
2023-02-16 14:48:16 +0100

    CHANGED folder structure

---
+++ test/config_tb.py
@@ -0,0 +1,67 @@
+"""Config file for testbench."""
+import sys
+import os.path
+from datetime import datetime as date
+
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
+
+# Data aquirer
+PATH_PAIRS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "pairs"))
+PAIR = "EURUSD"
+MINUTES = 1
+API_KEY = "kvtkOoyqcuTgNrBqRGIhhLe766CLYbpo"
+API_TYPE = "advanced"
+
+# Indicators
+PATH_INDICATORS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, f"indicators"))
+INDICATORS_DATA_SOURCE = f"{PATH_PAIRS}/{PAIR}_{MINUTES}.csv"
+TEST_INDICATORS = ["ATR", "BOLLINGER",'MA5' ,'MA25', "MA50", "MA200", "MACD", "OBV", "RSI", "STOCHASTIC", "VoRSI"]
+EXPECTED_COLUMNS = [
+    "v",
+    "vw",
+    "o",
+    "c",
+    "h",
+    "l",
+    "ATR",
+    "BOLLINGER_UPPER",
+    "BOLLINGER_MIDDLE",
+    "BOLLINGER_LOWER",
+    "MA5",
+    "MA25",
+    "MA50",
+    "MA200",
+    "MACD",
+    "MACD_SIGNAL",
+    "MACD_HIST",
+    "OBV",
+    "RSI",
+    "STOCHASTIC_K",
+    "STOCHASTIC_D",
+    "VoRSI",
+]
+TARGET = 'MA5'
+
+# Preprocessor
+PREPROCESSOR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "preprocessor_test"))
+PREPROCESSOR_DATA_SOURCE = f"{PATH_INDICATORS}/{PAIR}_{MINUTES}.csv"
+TEST_TIME_STEPS_IN = 6720  # 10 weeks
+TEST_TIME_STEPS_OUT = 672  # 1 day
+TEST_LENGTH = TEST_TIME_STEPS_IN + TEST_TIME_STEPS_OUT
+TEST_SCALE = True
+
+# Model
+MODEL_DATA_SOURCE = f"{PREPROCESSOR_DATA_SOURCE}"
+MODEL_PATH = os.path.abspath(os.path.dirname(__file__))
+MODEL_NAME = 'EURUSD_15'
+TEST_EPOCHS = 200
+TEST_NEURONS = 64
+TEST_BATCH_SIZE = 16
+TEST_LEARNING_RATE = 0.0005
+PATH_TEST_RESULTS = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "test_results"))
+
+# System test
+REQUEST_PAIRS = ['AUDCHF', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADCHF', 'CADJPY', 'CHFJPY', 'EURAUD', 'EURCAD', 'EURCHF', 'EURGBP', 'EURJPY', 'EURNZD', 'EURUSD', 'GBPAUD', 'GBPCAD', 'GBPCHF', 'GBPJPY', 'GBPNZD', 'GBPUSD', 'NZDCAD', 'NZDCHF', 'NZDJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
