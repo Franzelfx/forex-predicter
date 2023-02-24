9297ca294b57a9f1eb241b154b731e4fb61cb387
Franzelfx <fabian.franz0596@gmail.com>
2023-02-24 20:25:18 +0100

    EDIT config

--- test/config_tb.py
+++ test/config_tb.py
@@ -51,11 +51,11 @@ TARGET = 'MA5'
 # Preprocessor
 PREPROCESSOR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "preprocessor_test"))
 PREPROCESSOR_DATA_SOURCE = f"{PATH_INDICATORS}/{PAIR}_{MINUTES}.csv"
-TEST_TIME_STEPS_IN = 6720  # 10 weeks
+TEST_TIME_STEPS_IN = 6720  # 
 TEST_TIME_STEPS_OUT = 1440 # 1 day
 TEST_LENGTH = TEST_TIME_STEPS_IN + TEST_TIME_STEPS_OUT
 TEST_SCALE = True
-TEST_SHIFT = 720 # overlap of one means x and y wndows are shifted by one in evr sample
+TEST_SHIFT = TEST_TIME_STEPS_OUT # overlap of one means x and y wndows are shifted by one in evr sample
 
 # Model
 MODEL_DATA_SOURCE = f"{PREPROCESSOR_DATA_SOURCE}"
