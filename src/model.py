2979b3b1b7e92c99013050e2311496fded481f42
Franzelfx <fabian.franz0596@gmail.com>
2023-02-26 15:35:13 +0100

    EDIT model

--- src/model.py
+++ src/model.py
@@ -50,13 +50,15 @@ class Model:
             LSTM(
                 hidden_neurons,
                 return_sequences=True,
-                input_shape=(self._x_train.shape[0], self._x_train.shape[1], self._x_train.shape[2])
+                input_shape=(self._x_train.shape[1], self._x_train.shape[2])
             )
         )
+        model.add(LSTM(int(hidden_neurons), return_sequences=True))
         model.add(GRU(int(hidden_neurons), return_sequences=True))
+        model.add(LSTM(int(hidden_neurons), return_sequences=False))
         model.add(Dropout(dropout))
         model.add(Dense(self._y_train.shape[1], activation=activation))
-        model.add(Dense(self._y_train.shape[1]))
+        model.add(Dense(self._y_train.shape[1], activation='linear'))
         model.build(
             input_shape=(
                 self._x_train.shape[0],
