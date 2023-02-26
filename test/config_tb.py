35379b0e2af54d686880520ae02374dd99dffebb
Franzelfx <fabian.franz0596@gmail.com>
2023-02-26 04:53:49 +0100

    EDIT model config

--- test/config_tb.py
+++ test/config_tb.py
@@ -51,7 +51,7 @@ TARGET = 'MA5'
 # Preprocessor
 PREPROCESSOR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.curdir, "preprocessor_test"))
 PREPROCESSOR_DATA_SOURCE = f"{PATH_INDICATORS}/{PAIR}_{MINUTES}.csv"
-TEST_TIME_STEPS_IN = 7200 # 1 week
+TEST_TIME_STEPS_IN = 14400 # 1 week
 TEST_TIME_STEPS_OUT = 720 # 12 hours
 TEST_LENGTH = TEST_TIME_STEPS_IN + TEST_TIME_STEPS_OUT
 TEST_SCALE = True
