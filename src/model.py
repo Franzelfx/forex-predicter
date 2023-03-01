62cca8866531b6fe77e5b5b910c8dcec52986209
Franzelfx <fabian.franz0596@gmail.com>
2023-03-01 22:40:28 +0100

    REMOVED dropout

--- src/model.py
+++ src/model.py
@@ -67,9 +67,9 @@ class Model:
             )
         )
         model.add(GRU(int(hidden_neurons), return_sequences=False))
-        model.add(Dropout(dropout))
+        #model.add(Dropout(dropout))
         model.add(Dense(self._y_train.shape[1], activation=activation))
-        model.add(Dropout(dropout))
+        #model.add(Dropout(dropout))
         model.add(Dense(self._y_train.shape[1], activation="linear"))
         model.build(
             input_shape=(
