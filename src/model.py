d27a903968086839832f80f617a7c827e2ee8d6a
Franzelfx <fabian.franz0596@gmail.com>
2023-03-02 22:41:02 +0100

    EDIT model

--- src/model.py
+++ src/model.py
@@ -69,7 +69,9 @@ class Model:
         model.add(GRU(int(hidden_neurons), return_sequences=True))
         model.add(GRU(int(hidden_neurons), return_sequences=False))
         model.add(Dropout(dropout))
-        model.add(Dense(self._y_train.shape[1], activation=activation))
+        model.add(Dense(hidden_neurons, activation=activation))
+        model.add(Dropout(dropout))
+        model.add(Dense(hidden_neurons, activation=activation))
         model.add(Dropout(dropout))
         model.add(Dense(self._y_train.shape[1], activation="linear"))
         model.build(
