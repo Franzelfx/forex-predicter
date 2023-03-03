a11f9fa32e30e3642b88bab05f5ae82095f35f4f
Franzelfx <fabian.franz0596@gmail.com>
2023-03-03 01:07:29 +0100

    EDIT model

--- src/model.py
+++ src/model.py
@@ -72,9 +72,7 @@ class Model:
         model.add(Dense(hidden_neurons, activation=activation))
         model.add(Dropout(dropout))
         model.add(Dense(hidden_neurons, activation=activation))
-        model.add(Dropout(dropout))
         model.add(Dense(hidden_neurons, activation=activation))
-        model.add(Dropout(dropout))
         model.add(Dense(self._y_train.shape[1], activation="linear"))
         model.build(
             input_shape=(
