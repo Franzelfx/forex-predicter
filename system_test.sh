64562406ec58dd1caa47642378bd3115b1f1f59c
Franzelfx <fabian.franz0596@gmail.com>
2023-05-10 08:53:43 +0200

    EDIT system_test.sh

--- system_test.sh
+++ system_test.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 source venv/bin/activate
 export API_KEY=kvtkOoyqcuTgNrBqRGIhhLe766CLYbpo
+export START_PAIR=$1
+echo "API_KEY"=$API_KEY
+echo "START_PAIR="$1
 screen python test/system_test.py
