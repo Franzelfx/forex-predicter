0f2564cba16c1338fe1fd6a6781d91290f5d0115
Franzelfx <fabian.franz0596@gmail.com>
2023-05-16 16:14:25 +0200

    EDIT model

--- src/model.py
+++ src/model.py
@@ -67,6 +67,7 @@ class Model:
             )
         )
         model.add(Bidirectional(LSTM(round(0.5 * hidden_neurons), return_sequences=True)))
+        model.add(Bidirectional(LSTM(self._y_train.shape[1], return_sequences=True)))
         model.add(TimeDistributed(Dense(round(0.75 * hidden_neurons), activation='relu')))
         model.add(Dropout(dropout_factor))
         model.add(TimeDistributed(Dense(round(0.75 * hidden_neurons), activation='relu')))
@@ -74,7 +75,7 @@ class Model:
         model.add(TimeDistributed(Dense(round(0.5 * hidden_neurons), activation='relu')))
         model.add(TimeDistributed(Dense(round(0.5 * hidden_neurons), activation='relu')))
         model.add(TimeDistributed(Dense(round(0.25 * hidden_neurons), activation='relu')))
-        model.add(TimeDistributed(Dense(1, activation="linear")))
+        model.add(TimeDistributed(Dense(self._y_train.shape[1], activation="linear")))
         model.build(
             input_shape=(
                 self._x_train.shape[0],
