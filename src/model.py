174898af9a4510bec239126ba5410416612aadf6
Franzelfx <fabian.franz0596@gmail.com>
2023-05-17 00:29:38 +0200

    EDIT model

--- src/model.py
+++ src/model.py
@@ -82,7 +82,7 @@ class Model:
                     LSTM(
                         hidden_neurons,
                         return_sequences=True,
-                        input_shape=(self._x_train.shape[1], self._x_train.shape[2]),
+                        batch_input_shape=(self._x_train.shape[1], self._x_train.shape[2]),
                         stateful=stateful,
                     )
                 )
@@ -98,6 +98,7 @@ class Model:
         model.add(TimeDistributed(Dense(self._y_train.shape[1], activation=activation)))
         model.add(GlobalMaxPooling1D())
         model.add(Dense(self._y_train.shape[1], activation="linear"))
+        model.build()
         return model
 
     def _create_branched_model(
