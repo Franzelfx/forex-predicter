7a7673520fe3f9d36959045d781b0f218aa957e1
Franzelfx <fabian.franz0596@gmail.com>
2023-05-16 17:11:35 +0200

    EDIT model

--- src/model.py
+++ src/model.py
@@ -75,8 +75,8 @@ class Model:
         model.add(Dropout(dropout_factor))
         model.add(TimeDistributed(Dense(round(0.5 * hidden_neurons), activation='relu')))
         model.add(TimeDistributed(Dense(round(0.5 * hidden_neurons), activation='relu')))
-        model.add(TimeDistributed(Dense(round(0.25 * hidden_neurons), activation='relu')))
-        model.add(GlobalMaxPooling1D(self._y_train.shape[1]))
+        model.add(TimeDistributed(Dense(self._y_train.shape[1]), activation='relu'))
+        model.add(GlobalMaxPooling1D())
         model.add(Dense(1, activation="linear"))
         model.build(
             input_shape=(
