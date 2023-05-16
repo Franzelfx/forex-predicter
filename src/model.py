1a6160844043bffea5450d038c0013ec4b70b52d
Franzelfx <fabian.franz0596@gmail.com>
2023-05-17 00:42:36 +0200

    EDIT model

--- src/model.py
+++ src/model.py
@@ -98,7 +98,10 @@ class Model:
         model.add(TimeDistributed(Dense(self._y_train.shape[1], activation=activation)))
         model.add(GlobalMaxPooling1D())
         model.add(Dense(self._y_train.shape[1], activation="linear"))
-        model.build()
+        if(stateful):
+            model.build(input_shape=(batch_size, self._x_train.shape[1], self._x_train.shape[2]))
+        else:
+            model.build(input_shape=(self._x_train.shape[0], self._x_train.shape[1], self._x_train.shape[2]))
         model.compile(loss="mse", optimizer="adam")
         return model
 
