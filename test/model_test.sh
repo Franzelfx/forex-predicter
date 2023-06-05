b4a13168ee91f427fcfc775af4f0c7bbc9496045
Franzelfx <fabian.franz0596@gmail.com>
2023-06-06 00:39:51 +0200

    EDIT .sh

--- test/model_test.sh
+++ test/model_test.sh
@@ -5,6 +5,24 @@ export START_PAIR=$1
 echo "API_KEY"=$API_KEY
 echo "START_PAIR="$1
 # Configure the env variables
-./_config.sh
-# Run the test
-screen python model_test.py
+source ../venv/bin/activate
+# ------------------------------
+# Ask User, if multiple GPUs should be used
+read -p "Do you want to use multiple GPUs? (y/n): " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Multiple GPUs will be used"
+    export USE_MULTIPLE_GPUS="True"
+else
+    echo "Only one GPU will be used"
+    export USE_MULTIPLE_GPUS="False"
+fi
+
+read -p "Want to use pair data from saved file? (y/n): " answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Loading from saved file"
+    export LOAD_FROM_SAVED_FILE=true
+else
+    echo "Not loading from saved file"
+    export LOAD_FROM_SAVED_FILE=false
+fi
+python model_test.py
