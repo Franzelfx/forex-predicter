ddb89b751ad59f6aefb877d3e5bbe27dd3ddf19e
Franzelfx <fabian.franz0596@gmail.com>
2023-06-07 22:31:48 +0200

    EDIT utilizer_test.sh

--- test/utilizer_test.sh
+++ test/utilizer_test.sh
@@ -7,16 +7,7 @@ echo "START_PAIR="$1
 # Configure the env variables
 source ../venv/bin/activate
 # ------------------------------
-# Ask User, if multiple GPUs should be used
-read -p "Do you want to use multiple GPUs? (y/n): " answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo "Multiple GPUs will be used"
-    export USE_MULTIPLE_GPUS="True"
-else
-    echo "Only one GPU will be used"
-    export USE_MULTIPLE_GPUS="False"
-fi
-
+# Ask if want to use saved file
 read -p "Want to use pair data from saved file? (y/n): " answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
     echo "Loading from saved file"
