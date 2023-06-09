d4f521ffce43f6b248d2f91293f93a840f8842eb
Franzelfx <fabian.franz0596@gmail.com>
2023-06-10 01:02:47 +0200

    EDIT config

--- test/config_tb.py
+++ test/config_tb.py
@@ -28,8 +28,8 @@ PAIR = "C:CADJPY"
 MINUTES = 15
 START = "2018-01-01"
 # Substract 1 hour to get the last full hour
-END = (date.today() - timedelta(hours=1)).strftime("%Y-%m-%d")
-#END = "2023-01-01"
+#END = (date.today() - timedelta(hours=1)).strftime("%Y-%m-%d")
+END = "2023-01-01"
 API_TYPE = "advanced"
 
 # ---------------------------------- #
