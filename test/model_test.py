2f2d6d2176b9ed29c950fa36fdb5134f26c84dec
Franzelfx <fabian.franz0596@gmail.com>
2023-07-08 00:43:38 +0200

    EDIT model test

--- test/model_test.py
+++ test/model_test.py
@@ -33,22 +33,15 @@ class Test_Model(unittest.TestCase):
         )
         indicators = Indicators(PATH_INDICATORS, pair, target_pair, TEST_INDICATORS)
         indicator_data = indicators.calculate(save=True)
-        print(indicator_data.head())
-        # Get the common time values between the target pair and correlated pairs
-        common_time_values = pd.Index(target_pair["t"])
-        for corr_pair in corr_pairs:
-            common_time_values = common_time_values.intersection(pd.Index(corr_pair.x_train["t"]))
-
         # Preprocess the target pair
         preprocessor = Preprocessor(
-            target_pair,
+            indicator_data,
             TARGET,
             time_steps_in=TEST_TIME_STEPS_IN,
             time_steps_out=TEST_TIME_STEPS_OUT,
             scale=TEST_SCALE,
             shift=TEST_SHIFT,
         )
-
         # Get correlated pairs
         for corr_pair in CORR_PAIRS:
             api_data_corr = aquirer.get(
@@ -64,14 +57,10 @@ class Test_Model(unittest.TestCase):
                 PATH_INDICATORS, corr_pair, api_data_corr, TEST_INDICATORS
             )
             indicator_data_corr = indicators.calculate(save=False)
-
-            # Convert time values to a common format
-            indicator_data_corr["t"] = pd.to_datetime(indicator_data_corr["t"])
-            indicator_data["t"] = pd.to_datetime(indicator_data["t"])
-
-            # Filter the correlated pair to keep only common time values
-            indicator_data_corr = indicator_data_corr[indicator_data_corr["t"].isin(common_time_values)]
-
+            # get a series of unique values from the 't' column of indicator_data
+            values_to_keep = indicator_data['t'].unique()
+            # filter rows of indicator_data_corr, keeping only those where 't' exists in values_to_keep
+            indicator_data_corr = indicator_data_corr[indicator_data_corr['t'].isin(values_to_keep)]
             # Preprocess the correlated pair
             preprocessor_corr = Preprocessor(
                 indicator_data_corr,
@@ -84,22 +73,6 @@ class Test_Model(unittest.TestCase):
             # Append to the list of correlated pairs
             corr_pairs.append(preprocessor_corr)
         corr_pairs.append(preprocessor)
-
-        # Check the lengths before and after preprocessing
-        target_length_before = len(target_pair)
-        target_length_after = len(preprocessor.x_train)
-        print(f"Target pair length (before preprocessing): {target_length_before}")
-        print(f"Target pair length (after preprocessing): {target_length_after}")
-
-        for i, corr_pair in enumerate(corr_pairs):
-            corr_length_before = len(corr_pair.x_train)
-            corr_pair.preprocess()
-            corr_length_after = len(corr_pair.x_train)
-            print(f"Correlated pair {i+1} length (before preprocessing): {corr_length_before}")
-            print(f"Correlated pair {i+1} length (after preprocessing): {corr_length_after}")
-        corr_pairs.append(preprocessor)
-        # Check if all dataframes have the same length
-        print(len(target_pair))
         # Model
         model = Model(
             MODEL_PATH,
