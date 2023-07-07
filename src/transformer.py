dc2da02c0671d686f67ca657ff539b2d64679979
Franzelfx <fabian.franz0596@gmail.com>
2023-07-07 14:08:56 +0200

    EDIT transformer block

--- src/transformer.py
+++ src/transformer.py
@@ -2,41 +2,38 @@ import tensorflow as tf
 from keras.layers import LayerNormalization, MultiHeadAttention, Dropout, Dense, Add
 
 class TransformerBlock(tf.keras.layers.Layer):
-    def __init__(self, hidden_neurons, attention_heads, dropout_rate):
+    def __init__(self, hidden_neurons, num_attention_heads, key_dim, dropout_rate):
         super(TransformerBlock, self).__init__()
         self.hidden_neurons = hidden_neurons
-        self.attention_heads = attention_heads
+        self.num_attention_heads = num_attention_heads
+        self.key_dim = key_dim
         self.dropout_rate = dropout_rate
 
-    def build(self, input_shape):
-        self.query_branch = Dense(self.hidden_neurons)
-        self.value_branch = Dense(self.hidden_neurons)
+        self.attention = MultiHeadAttention(
+            num_attention_heads, key_dim=key_dim
+        )
+        self.dropout1 = Dropout(dropout_rate)
+        self.norm1 = LayerNormalization()
 
-        self.attention = MultiHeadAttention(self.attention_heads, self.hidden_neurons)  # Make sure hidden_neurons match here
-        self.dropout_attention = Dropout(self.dropout_rate)
-        self.norm_attention = LayerNormalization()
+        self.feed_forward = self.build_feed_forward()
+        self.dropout2 = Dropout(dropout_rate)
+        self.norm2 = LayerNormalization()
 
-        self.feed_forward_1 = Dense(self.hidden_neurons, activation="relu")
-        self.feed_forward_2 = Dense(self.hidden_neurons, activation="relu")
-        self.feed_forward_3 = Dense(self.hidden_neurons, activation="relu")
-
-        self.dropout_ffn = Dropout(self.dropout_rate)
-        self.norm_ffn = LayerNormalization()
+    def build_feed_forward(self):
+        return tf.keras.Sequential(
+            [
+                Dense(self.hidden_neurons, activation="relu"),
+                Dense(self.hidden_neurons),
+            ]
+        )
 
     def call(self, inputs, training=False):
-        query = self.query_branch(inputs)
-        value = self.value_branch(inputs)
+        attention_output = self.attention(inputs, inputs)
+        attention_output = self.dropout1(attention_output, training=training)
+        attention_output = self.norm1(inputs + attention_output)
 
-        attention_output = self.attention(query, value)
-        attention_output = self.dropout_attention(attention_output, training=training)
-        residual_attention = Add()([inputs, attention_output])
-        norm_attention = self.norm_attention(residual_attention)
+        feed_forward_output = self.feed_forward(attention_output)
+        feed_forward_output = self.dropout2(feed_forward_output, training=training)
+        block_output = self.norm2(attention_output + feed_forward_output)
 
-        feed_forward_output = self.feed_forward_1(norm_attention)
-        feed_forward_output = self.feed_forward_2(feed_forward_output)
-        feed_forward_output = self.feed_forward_3(feed_forward_output)
-        feed_forward_output = self.dropout_ffn(feed_forward_output, training=training)
-        residual_ffn = Add()([norm_attention, feed_forward_output])
-        norm_ffn = self.norm_ffn(residual_ffn)
-
-        return norm_ffn
+        return block_output
