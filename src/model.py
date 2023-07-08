d71d4d5c677beab00edec560c4693e0f5e6ee908
Franzelfx <fabian.franz0596@gmail.com>
2023-07-08 02:45:39 +0200

    EDIT model

--- src/model.py
+++ src/model.py
@@ -302,7 +302,7 @@ class Model:
         branches = []
         for branch in architecture.branches:
             _input = Input(shape=branch.input.shape)
-            _branch = BranchLayer(branch.transformer_neurons, branch.lstm_neurons, branch.dense_neurons, branch.attention_heads, branch.dropout_rate, branch.is_input)(_input)
+            _branch = BranchLayer(branch.transformer_neurons, branch.lstm_neurons, branch.dense_neurons, branch.attention_heads, branch.dropout_rate)(_input)
             branches.append(_branch)
         # Summation layer
         summation = Add()(branches)
