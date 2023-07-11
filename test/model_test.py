ec747d746e6fe4fc6b88d86ac3b76b93ff88cd07
Franzelfx <fabian.franz0596@gmail.com>
2023-07-11 18:09:44 +0200

    EDIT model test

--- test/model_test.py
+++ test/model_test.py
@@ -142,7 +142,7 @@ class Test_Model(unittest.TestCase):
                 epochs=TEST_EPOCHS,
                 batch_size=TEST_BATCH_SIZE,
                 validation_split=TEST_VALIDATION_SPLIT,
-                patience=TEST_PATIENCE
+                patience=TEST_PATIENCE,
                 strategy=strategy if use_multiple_gpus == 'True' else None,
             )
         else:
