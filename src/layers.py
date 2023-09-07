cfa281de80b558e75a83275d5f107f743b0defbf
Franzelfx <fabian.franz0596@gmail.com>
2023-09-07 22:52:45 +0200

    EDIT relu to tanh

--- src/layers.py
+++ src/layers.py
@@ -19,18 +19,18 @@ class TransformerBlock(tf.keras.layers.Layer):
         self.attention_heads = attention_heads
         self.dropout_rate = dropout_rate
 
-        self.dense_1 = Dense(hidden_neurons, activation='relu')
-        self.dense_2 = Dense(hidden_neurons, activation='relu')
-        self.dense_3 = Dense(hidden_neurons, activation='relu')
+        self.dense_1 = Dense(hidden_neurons, activation='tanh')
+        self.dense_2 = Dense(hidden_neurons, activation='tanh')
+        self.dense_3 = Dense(hidden_neurons, activation='tanh')
         self.multihead_attention = MultiHeadAttention(attention_heads, hidden_neurons)
         self.dropout_attention = Dropout(dropout_rate)
         self.concat_attention = Concatenate()
         self.layer_norm_1 = LayerNormalization()
         
         # Feed forward layers
-        self.dense_ffn_1 = Dense(hidden_neurons, activation='relu')
-        self.dense_ffn_2 = Dense(hidden_neurons, activation='relu')
-        self.dense_ffn_3 = Dense(hidden_neurons, activation='relu')
+        self.dense_ffn_1 = Dense(hidden_neurons, activation='tanh')
+        self.dense_ffn_2 = Dense(hidden_neurons, activation='tanh')
+        self.dense_ffn_3 = Dense(hidden_neurons, activation='tanh')
 
         self.dropout_ffn = Dropout(dropout_rate)
         self.concat_ffn = Concatenate()
@@ -91,7 +91,7 @@ class TransformerLSTMBlock(tf.keras.layers.Layer):
             neurons_transformer, attention_heads, dropout_rate
         )
         self.lstm_layer = LSTM(neurons_lstm, return_sequences=True)
-        self.lstm_match = Dense(neurons_lstm, activation="relu")
+        self.lstm_match = Dense(neurons_lstm, activation="tanh")
         self.concat = Concatenate()
 
     def call(self, input_tensor):
@@ -243,10 +243,10 @@ class Output(tf.keras.layers.Layer):
         self.dense_layers = []
         self.dropout_layers = []
         self.gap = GlobalAveragePooling1D()
-        self.output_layer = Dense(output_neurons, activation="relu")
+        self.output_layer = Dense(output_neurons, activation="tanh")
 
         for neurons, dropout in zip(self.neurons_dense, self.dropout_rate):
-            dense_layer = Dense(neurons, activation="relu")
+            dense_layer = Dense(neurons, activation="tanh")
             dropout_layer = Dropout(dropout)
             self.dense_layers.append(dense_layer)
             self.dropout_layers.append(dropout_layer)
