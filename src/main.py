b27b1a7428ca4432147da462f84066605274bcec
Franzelfx <fabian.franz0596@gmail.com>
2023-12-21 16:51:13 +0100

    ADDED cors

--- src/main.py
+++ src/main.py
@@ -9,14 +9,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allow all origins, replace with your domain(s)
-#     allow_methods=["*"],  # Allow all HTTP methods
-#     allow_headers=["*"],  # Allow all HTTP headers
-#     allow_credentials=True,  # Allow sending cookies and credentials
-#     expose_headers=["*"],  # Expose all response headers
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, replace with your domain(s)
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all HTTP headers
+    allow_credentials=True,  # Allow sending cookies and credentials
+    expose_headers=["*"],  # Expose all response headers
+)
 app.include_router(router, prefix="/v1", tags=["routes"])
 
 def get_config():
