b5c9c492064decaca7dc9f8b4643dd781835a9fa
Franzelfx <fabian.franz0596@gmail.com>
2024-02-13 21:59:59 +0100

    EDIT indicator parallel computing

--- src/composer.py
+++ src/composer.py
@@ -352,7 +352,7 @@ class Composer:
 
             # Calculate the indicators
             #data = indicator.calculate(save=save)
-            data = indicator.calculate_indicators_in_parallel(save=save)
+            data = indicator.calculate_indicators_in_parallel()
             # Append the dataframes to the indicators list
             indicators.append(data)
         self._indicators = indicators
