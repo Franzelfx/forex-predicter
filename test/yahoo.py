4c3608b417169ca8381ea2ad05fb88acbdcee87f
franzelfx <fabian-franz@ffengineering.de>
2024-10-20 12:29:41 +0200

    delete 15 files

--- test/yahoo.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yahooFinance
-
-GetFacebookInformation = yahooFinance.Ticker("META")
-
-print(GetFacebookInformation.info)
-# Write 2 months of 15 min financial data to csv file
-GetFacebookInformation.history(period="2mo", interval="15m").to_csv("facebook.csv")
